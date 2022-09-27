bbe43333a7f97570236a0478128f1896cf817f47
stephaniepuertas <puertasstephanie@gmail.com>
2022-09-27 11:45:12 -0700

    finished print_info iteration

--- nested_dictionaries_and_list.py
+++ nested_dictionaries_and_list.py
@@ -53,6 +53,7 @@ def iterateDictionary(some_list):
             print (key,item[key])
 iterateDictionary(students)
 
+# another way to write the same thing
 # def iterateStudents():
 #     for student in students:
 #         for name in student:
@@ -64,18 +65,19 @@ iterateDictionary(students)
 # # should output: (it's okay if each key-value pair ends up on 2 separate lines;
 
 
-# # bonus to get them to appear exactly as below!)
-# # first_name - Michael, last_name - Jordan
-# # first_name - John, last_name - Rosales
-# # first_name - Mark, last_name - Guillen
-# # first_name - KB, last_name - Tonel
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
 
 
 # # 3- Get Values From a List of Dictionaries
-#     # Create a function iterateDictionary2(key_name, some_list) 
-#     # that, given a list of dictionaries and a key name, the function prints the value 
-#     # stored in that key for each dictionary. 
-#     For example, iterateDictionary2('first_name', students) should output:
+
+# Create a function iterateDictionary2(key_name, some_list) 
+# that, given a list of dictionaries and a key name, the function prints the value 
+# stored in that key for each dictionary. 
+# For example, iterateDictionary2('first_name', students) should output:
 
 
 # def iterateDictionary(some_list):
@@ -84,30 +86,48 @@ iterateDictionary(students)
 #             print (key,item[key])
 # iterateDictionary(students)
 
-# DEFINE FUNCTION
-def iterateDictionary2(key_name, students):
-    for student in students:
+# # DEFINE FUNCTION
+def iterateDictionary2(key_name, some_list):
+    for student in some_list:
         print(key_name,student[key_name])
 
 
-# CALL FUNCTOIN
+# # CALL FUNCTOIN
 iterateDictionary2('first_name', students)
 iterateDictionary2('last_name', students)
 
 
-
-# # 4 -Iterate Through a Dictionary with List Values
-#     # Create a function printInfo(some_dict) that given a dictionary whose values 
-#     # are all lists, prints the name of each key along with the size of its list, 
-#     # and then prints the associated values within each key's list. For example:
-
-
+# # # 4 -Iterate Through a Dictionary with List Values
+# #     # Create a function printInfo(some_dict) that given a dictionary whose values 
+# #     # are all lists, prints the name of each key along with the size of its list, 
+# #     # and then prints the associated values within each key's list. For example:
 
 dojo = {
-    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+    'locations': [
+        'San Jose',
+        'Seattle',
+        'Dallas',
+        'Chicago',
+        'Tulsa',
+        'DC',
+        'Burbank'],
+    'instructors':[
+        'Michael',
+        'Amy',
+        'Eduardo',
+        'Josh',
+        'Graham',
+        'Patrick',
+        'Minh',
+        'Devon']
 }
 
+def print_info(dict_input):
+    for key in dict_input.keys():
+        print(f'{len(dict_input[key])}{key}')
+        for element in dict_input[key]:
+            print(element)
+print_info(dojo)
 
 
 # # printInfo(dojo)
